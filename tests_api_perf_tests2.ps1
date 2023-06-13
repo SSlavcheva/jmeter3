35e9aead182d2ea2370ead76e6cfad998a68c9de
Silviya Slavcheva <s.slavcheva@dynamosoftware.com>
2023-06-13 10:13:22 +0300

    change agent directory

--- tests_api_perf_tests2.ps1
+++ tests_api_perf_tests2.ps1
@@ -1,5 +1,5 @@
 
-& "C:\Users\s.slavcheva\Desktop\Demo\jmeterProjectDir\apache-jmeter-5.5\apache-jmeter-5.5\bin" -n -t "C:\Users\s.slavcheva\Desktop\Demo\jmeterProjectDir\apache-jmeter-5.5\apache-jmeter-5.5\bin\test.jmx" -l "C:\BuildAgent\work\be8f35a5e987082e\Logs.jtl"
+& "C:\Users\s.slavcheva\Desktop\Demo\jmeterProjectDir\apache-jmeter-5.5\apache-jmeter-5.5\bin" -n -t "C:\Users\s.slavcheva\Desktop\Demo\jmeterProjectDir\apache-jmeter-5.5\apache-jmeter-5.5\bin\test.jmx" -l "C:\BuildAgent\work\95513c9542ca730d\Logs.jtl"
 
 
 
