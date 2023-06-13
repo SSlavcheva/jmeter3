8635ad3bc66a2a9df94d3cd450a0e038d2329be4
Silviya Slavcheva <s.slavcheva@dynamosoftware.com>
2023-06-13 10:39:58 +0300

    test

--- tests_api_perf_tests2.ps1
+++ tests_api_perf_tests2.ps1
@@ -1,5 +1,5 @@
 
-& "C:\Users\s.slavcheva\Desktop\Demo\jmeterProjectDir\apache-jmeter-5.5\apache-jmeter-5.5\bin" -n -t "C:\Users\s.slavcheva\Desktop\Demo\jmeterProjectDir\apache-jmeter-5.5\apache-jmeter-5.5\bin\test.jmx" -l "C:\BuildAgent\work\95513c9542ca730d\Logs.jtl"
+& "C:\Users\s.slavcheva\Desktop\Demo\jmeterProjectDir\apache-jmeter-5.5\apache-jmeter-5.5\bin\jmeter.bat" -n -t "C:\Users\s.slavcheva\Desktop\Demo\jmeterProjectDir\apache-jmeter-5.5\apache-jmeter-5.5\bin\test.jmx" -l "C:\BuildAgent\work\95513c9542ca730d\Logs.jtl"
 
 
 
