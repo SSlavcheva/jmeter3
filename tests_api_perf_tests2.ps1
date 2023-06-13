ea2f7e42d1c556d9121d2c80bdd9b35cba68d2de
Silviya Slavcheva <s.slavcheva@dynamosoftware.com>
2023-06-13 09:54:44 +0300

    test

---
+++ tests_api_perf_tests2.ps1
@@ -0,0 +1,15 @@
+
+& "C:\Users\s.slavcheva\Desktop\Demo\jmeterProjectDir\apache-jmeter-5.5\apache-jmeter-5.5\bin" -n -t "C:\Users\s.slavcheva\Desktop\Demo\jmeterProjectDir\apache-jmeter-5.5\apache-jmeter-5.5\bin\test.jmx" -l "C:\BuildAgent\work\be8f35a5e987082e\Logs.jtl"
+
+
+
+Write-Output "##teamcity[blockOpened name='script is run']"
+
+
+
+
+
+
+
+
+
